5ae8a6f4c68a9188b6a9159054577763e1571e1a
Joao Pedro Teixeira Pereira de Sa <up201506252@fe.up.pt>
2018-01-14 01:30:26 +0000

    RegExp in HTML & JS

---
+++ Exercises/RegEx/8/script.js
@@ -0,0 +1,11 @@
+window.addEventListener('keypress', validatePassword, false); //false - bubbling phase
+
+function validatePassword(){
+  let password, username;
+  password = document.getElementById('password');
+  username = document.getElementById('username');
+
+  /*
+  do stuff
+  */
+}
